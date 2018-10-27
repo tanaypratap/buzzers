62ad4c92da8d19a49c641a1de6dbb19192c00ad2
Akanksha Choudhary <akanksha.ch29@gmail.com>
2018-10-27 14:53:14 +0530

    Material UIinstalled- ready to use

--- src/quiz/QuizLayout.js
+++ src/quiz/QuizLayout.js
@@ -3,15 +3,35 @@
  * @author Akanksha Choudhary <akanksha.ch29@gmail.com>
  */
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Quiz.css'
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  }
+});
 
 class QuizLayout extends React.Component {
     componentDidMount() {
         console.log('This needs to be removed');
     }
     render() {
-        return <h1 className="quiz-number-header"> Quiz no. {this.props.quizNumber} </h1>
+        // const { classes } = props;
+        return (
+          // <h1 className="quiz-number-header"> Quiz no. {this.props.quizNumber} </h1>
+          <Button variant="contained" color="primary" style={styles.button}>
+            Test Button
+          </Button>
+        )
     }
 }
 
-export default QuizLayout
+QuizLayout.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+
+export default withStyles(styles)(QuizLayout)
