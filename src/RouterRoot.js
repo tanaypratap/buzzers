79aad147a7c80ef17313c80ca46fddb00d68eefb
Tanay Pratap <tanaypratap@Tanays-MacBook-Pro.local>
2018-10-27 19:14:04 +0530

    router add

--- src/RouterRoot.js
+++ src/RouterRoot.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import QuizContainer from './quiz';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import QuizContainer from './components/quiz';
+import App from './components/testapp';
 
 function Root(){
     return(
         <Router>
-            <Route exact path="/" component={QuizContainer} />
+            <Switch>
+                <Route exact path="/" component={QuizContainer} />
+                <Route path="/test-app" component={App} />
+            </Switch>
         </Router>
     )
 }
