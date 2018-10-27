fd964e13c63e9e81a50dc92f3d2be8236acc5a86
Akanksha Choudhary <akanksha.ch29@gmail.com>
2018-10-27 16:44:41 +0530

    Quiz Page in progress

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import Quiz from './quiz';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import theme from './utils/material-ui-theme'
 
 class App extends Component {
   render() {
@@ -13,4 +15,12 @@ class App extends Component {
   }
 }
 
-export default App;
+function ThemedApp(){
+  return (
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>
+  )
+}
+
+export default ThemedApp;
--- src/quiz/QuizLayout.js
+++ src/quiz/QuizLayout.js
@@ -7,8 +7,20 @@ import PropTypes from 'prop-types';
 import './Quiz.css'
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
 
 const styles = theme => ({
+  card: {
+    maxWidth: 200,
+  },
+  media: {
+    height: 140,
+  },
   button: {
     margin: theme.spacing.unit,
   }
@@ -19,10 +31,34 @@ class QuizLayout extends React.Component {
         console.log('This needs to be removed');
     }
     render() {
+      const { classes } = this.props;
         return (
-          <Button variant="contained" color="primary" style={styles.button}>
-            Test Button
-          </Button>
+          <div className="container text-center">
+            <div className="row text-center">
+              <Card className={classes.card}>
+                <CardActionArea>
+                  <CardMedia
+                    style={styles.media}
+                    image="/images/gk_thumbnail.jpg"
+                    title="Competitive"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      General Knowledge
+                    </Typography>
+                    <Typography component="p">
+                      Test your general knowledge
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button variant="contained" color="primary" style={styles.button}>
+                    Start Quiz
+                  </Button>
+                </CardActions>
+              </Card>
+            </div>
+          </div>
         )
     }
 }
