9972f56464d9ae7ad753c8af3cdac2febb1ee0a6
akankshach29 <31201111+akankshach29@users.noreply.github.com>
2018-10-28 12:45:20 +0530

    Akanksha/questions ui (#13)
    * Questions Page in progress
    
    * Quiz Questions pushed to components folder
    
    * Delete App.js
    
    * Merge branch 'master' of https://github.com/Hackbit/nko2018-buzzers into akanksha/quiz-questions
    
    * Quizzes page design

--- src/components/quiz/containers/PlayArena.js
+++ src/components/quiz/containers/PlayArena.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import { getQuizQuestion } from "../../../firebase-utils/firebase-client";
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+    padding: '20px',
+    width: '100%',
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: '20px',
+  }
+});
 
 class PlayArena extends Component{
     constructor(props){
@@ -47,7 +61,7 @@ class PlayArena extends Component{
         let message= "";
         let currentQuestion = this.state.currentQuestion;
         (answer === this.state.question.correctAnswer)?
-            message="Correct":    
+            message="Correct":
             message="Incorrect";
         this.setState({
             canAnswer: false,
@@ -57,48 +71,68 @@ class PlayArena extends Component{
     }
 
     render(){
+        const { classes } = this.props;
         return(
-            <div>
-                {!this.state.canAnswer && 
-                    <h4>Remaining Time for next question: 5</h4>
+            <div className="container" style={{ backgroundColor: '#2f0338', minHeight: '100vh' }}>
+              <div className="row" style={{ paddingTop: '5vh' }}>
+                {
+                  !this.state.canAnswer &&
+                    <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap', alignItems: 'center' }}>
+                      <Typography gutterBottom variant="h5" component="h2" style={{ color: '#ffffff' }}>
+                        Remaining Time for next question: 5
+                      </Typography>
+                    </div>
                 }
                 {this.state.question ?
-                    <div>
-                        <h3>Question Statement: {this.state.question.questionText}</h3>
-                        <Button variant="contained" color="primary" 
+                    <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
+                        <div style={{ height: '200px', display: 'flex', justifyContent: 'center', flexWrap: 'wrap', alignItems: 'center', padding: '15px' }}>
+                          <Typography gutterBottom variant="h5" component="h1" style={{ color: 'white' }}>
+                            Question: {this.state.question.questionText}
+                          </Typography>
+                        </div>
+                        <Button variant="outlined" className={classes.button} type="button"
                             onClick={ (event) => this.handleClick(event, this.state.question.option1)}
                             disabled={!this.state.canAnswer}>
                             {this.state.question.option1}
-                        </Button>   
+                        </Button>
                         <br /><br />
-                        <Button variant="contained" color="primary" 
+                        <Button variant="outlined" className={classes.button} type="button"
                             onClick={ (event) => this.handleClick(event, this.state.question.option2)}
                             disabled={!this.state.canAnswer}>
                             {this.state.question.option2}
-                        </Button>   
+                        </Button>
                         <br /><br />
-                        <Button variant="contained" color="primary" 
+                        <Button variant="outlined" className={classes.button} type="button"
                             onClick={ (event) => this.handleClick(event, this.state.question.option3)}
                             disabled={!this.state.canAnswer}>
                             {this.state.question.option3}
-                        </Button>   
+                        </Button>
                         <br /><br />
-                        <Button variant="contained" color="primary" 
+                        <Button variant="outlined" className={classes.button} type="button"
                             onClick={ (event) => this.handleClick(event, this.state.question.option4)}
                             disabled={!this.state.canAnswer}>
                             {this.state.question.option4}
-                        </Button>                
+                        </Button>
                     </div>:
-                    <div>
-                        <h4>Time for Quiz to start: 5</h4>
+                    <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap', alignItems: 'center', padding: '20px', paddingTop: '40vh' }}>
+                      <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap', alignItems: 'center' }}>
+                        <Typography gutterBottom variant="h3" component="h2" style={{ color: '#ffffff' }}>
+                          Time for Quiz to start: 5
+                        </Typography>
+                      </div>
                     </div>
                 }
-                {!this.state.canAnswer && 
+                {!this.state.canAnswer &&
                     <h4>{this.state.message}</h4>
                 }
+              </div>
             </div>
         )
     }
 }
 
-export default PlayArena;
+PlayArena.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(PlayArena)
