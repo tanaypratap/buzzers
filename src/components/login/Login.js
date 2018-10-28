e315360d3216ca4d981483731b5feff5d8366b4d
Tanay Pratap <tanaypratap@Tanays-MacBook-Pro.local>
2018-10-28 17:59:26 +0530

    merge with master

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import * as firebaseui from 'firebaseui'
+import firebase from '../../firebase';
 
 class Login extends React.Component {
-    render() {
-        return <h1> Login logic will come here.</h1>
+    componentDidMount() {
+        this.enableFirebaseAuth.bind(this)
     }
+
+    enableFirebaseAuth() {
+        const uiConfig = { signInSuccessUrl: "http://localhost:3000/", signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID] };
+        const ui = new firebaseui.auth.AuthUI(firebase.auth());
+        ui.start('#firebaseui-auth-container', uiConfig);
+    }
+    render() {
+        return <div id="firebaseui-auth-container"></div>
+     }
 }
 
 export default Login
