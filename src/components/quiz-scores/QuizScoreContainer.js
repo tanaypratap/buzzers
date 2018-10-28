a1f5db70186157601dba9727c4a013400cba89cc
Shashank Shekhar <shashank483384@outlook.com>
2018-10-28 23:04:36 +0530

    Added Few features

--- src/components/quiz-scores/QuizScoreContainer.js
+++ src/components/quiz-scores/QuizScoreContainer.js
@@ -5,14 +5,31 @@
 import React from 'react';
 import QuizScore from './QuizScore';
 import { withRouter } from "react-router-dom";
+import { getFinalUserScore } from '../../firebase-utils/firebase-client';
 
 class QuizScoreContainer extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            finalScore: 0
+        }
+    }
     componentDidMount() {
+        const quizId = localStorage.getItem('qId');
+        console.log(quizId);
+        const user = JSON.parse(localStorage.getItem('user'));
+        console.log(user);
+        getFinalUserScore(quizId, user, (val) => {
+            console.log(val);
+            this.setState({
+                finalScore: val
+            })
+        })
     }
     render() {
         return (
             <div>
-                <QuizScore numberofQuestionsAnswered = {5} winnersList = {['a', 'b']} />
+                <QuizScore numberofQuestionsAnswered = {this.state.finalScore} winnersList = {['a', 'b']} />
             </div>
         )
     }
