e97acdf9ff6c060af403cfa5f1612e532df0b6de
Shashank Shekhar <shashe@microsoft.com>
2018-12-09 22:31:15 +0530

    Resolving comments part I

--- src/components/quiz-start-timer/QuizStartTimerContainer.js
+++ src/components/quiz-start-timer/QuizStartTimerContainer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import QuizStartTimer from './QuizStartTimer'
 import moment from 'moment';
-import { getQuiz, quizIndex } from './../../firebase-utils/firebase-client';
+import * as firebase from './../../firebase-utils/firebase-client';
 import { withRouter } from "react-router-dom";
 
 class QuizStartTimerContainer extends React.Component {
@@ -19,7 +19,16 @@ class QuizStartTimerContainer extends React.Component {
         const { quizId } = this.props.match.params;
         localStorage.setItem('qId', quizId);
         // Gets the quiz from firebase and users who have joined
-        getQuiz(quizId, (val) => {
+        this.startGame(quizId);
+    }
+
+    componentWillUnmount() {
+        // Clearing setTimeout
+        clearTimeout(this.timerId);
+    }
+
+    startGame = (quizId) => {
+        this.getQuiz(quizId).then( (val) => {
             // Calculates remaining time
             const remainingTime = moment(val.Start_time).fromNow();
             const remainingTimeInMilliSeconds = val.Start_time - Date.now();
@@ -33,9 +42,12 @@ class QuizStartTimerContainer extends React.Component {
         });
     }
 
-    componentWillUnmount() {
-        // Clearing setTimeout
-        clearTimeout(this.timerId);
+    getQuiz = (quizId) => {
+        return new Promise( (resolve, reject) => {
+            firebase.getQuiz(quizId, (val) => {
+                resolve(val);
+            })
+        })
     }
 
     render() {
--- src/components/testapp/actions/actionTypes.js
+++
@@ -1,6 +0,0 @@
-/**
- * @author Shashank
- * @description  All Action types are defined 
- */
-export const FETCH_STUFF = 'FETCH_STUFF';
-export const RECEIVE_STUFF = 'RECEIVE_STUFF';
